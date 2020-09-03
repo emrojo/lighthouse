9a7c64e2d73a177962251fbf25f5a99b31106049
Stephen Inglis <si5@sanger.ac.uk>
2020-09-03 15:42:23 +0100

    Reset development.py

--- lighthouse/config/development.py
+++ lighthouse/config/development.py
@@ -6,18 +6,8 @@ from lighthouse.config.defaults import *  # noqa: F403, F401
 SCHEDULER_RUN = False
 
 # Eve config
-# MONGO_HOST = "127.0.0.1"
-# MONGO_DBNAME = "lighthouseDevelopmentDB"
-
-MONGO_HOST = "vm-mg-psd-uat1.internal.sanger.ac.uk"
-MONGO_DBNAME = "lighthouseUATDB"
-MONGO_URI = (
-    "mongodb://lighthouse_owner:ora7gepe7@vm-mg-psd-uat1.internal.sanger.ac.uk/lighthouseUATDB"
-)
-# MLWH_CONN_STRING: "mlwhd_admin:jEbRepuJe7@mlwh-db:3436"
-MLWH_CONN_STRING: "mlwh_admin:pESatUpr8S@mlwh-db:3435"
-ML_WH_DB = "mlwhd_mlwarehouse_devdata"
-EVENTS_WH_DB = "mlwhd_mlwh_events_proddata"
+MONGO_HOST = "127.0.0.1"
+MONGO_DBNAME = "lighthouseDevelopmentDB"
 
 # logging config
 LOGGING["loggers"]["lighthouse"]["level"] = "DEBUG"  # noqa: F405
