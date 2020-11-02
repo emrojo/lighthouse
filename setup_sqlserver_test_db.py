a3cd481254b72b9be709d5b01cee0205cf5c2b67
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-11-02 00:11:37 +0000

    Test

--- setup_sqlserver_test_db.py
+++ setup_sqlserver_test_db.py
@@ -1,5 +1,5 @@
 import lighthouse.config.test as config  # type: ignore
-import pyodbc
+import pyodbc  # type: ignore
 
 cnxn = pyodbc.connect(
     "DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:localhost;DATABASE=master;UID=SA;PWD=MyV3rY@7742w0rd",
